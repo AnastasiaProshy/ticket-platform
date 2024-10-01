364154447155b77dbbd05fbba3d4a863009f5636
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-10-01 22:22:16 +0200

    Add Note entity with relationships to Ticket and User

---
+++ src/main/java/com/java/platform/model/Note.java
@@ -0,0 +1,77 @@
+package com.java.platform.model;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "notes")
+public class Note 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String text;
+	
+	@NotNull
+	private Timestamp createdAt;
+	
+	@ManyToOne
+	@JoinColumn(name = "ticket_id")
+	private Ticket ticket;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Ticket getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(Ticket ticket) {
+		this.ticket = ticket;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
