f484e50f59723c2c803c3da9945953f48093be72
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-10-01 22:22:59 +0200

    Add Status entity with validation constraints

---
+++ src/main/java/com/java/platform/model/Status.java
@@ -0,0 +1,41 @@
+package com.java.platform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "states")
+public class Status {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	private String status;
+
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}
